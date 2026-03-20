85f6c36bfb1e2250e04fca1d42811388a5a1167d
beytuygt <112018938+beytuygt@users.noreply.github.com>
2026-03-20 02:54:54 +0100

    Update

--- utils.py
+++ utils.py
@@ -6,4 +6,11 @@ def load_data():
     return pd.read_parquet("Data/FULL_DATASET_GKG.parquet")
 
 def load_data_rq4():
-    return pd.read_parquet("Data/gkg_partitioned_full_2021.parquet")
+    parts = [
+        "Data/gkg_partitioned_2021_part1.parquet",
+        "Data/gkg_partitioned_2021_part2.parquet",
+        "Data/gkg_partitioned_2021_part3.parquet",
+        "Data/gkg_partitioned_2021_part4.parquet"
+    ]
+    df_list = [pd.read_parquet(p) for p in parts]
+    return pd.concat(df_list, ignore_index=True)
